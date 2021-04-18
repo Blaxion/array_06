2f8e12fbf1e52ccb8f7390860a5063a7d599921f
Blaxion <khibaziev.f@gmail.com>
2021-04-18 16:47:07 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,47 @@
+// Exo 1
+// let monTab = ['pomme', 'fraise', 'melon', 'citron'];
+// monTab.shift();
+// console.log (monTab);
+// monTab.push("banane");
+// console.log (monTab);
+// monTab.unshift("cerise");
+// console.log (monTab);
+// monTab[1]= monTab[1].toUpperCase();
+// console.log (monTab);
+
+// Exo 2
+// let chiffres = [4, 5, 2, 1, 3];
+// chiffres.sort();
+// console.log(chiffres);
+// chiffres = chiffres.join(`-`);
+// console.log(chiffres);
+// chiffres = chiffres.split(`-`);
+// console.log(chiffres);
+
+// Exo 3
+// let x = [];
+
+// x.push(1,2,3,4,5,6,7,8,9,10);
+// console.log(x , x.length);
+// console.log(x.indexOf(1));
+// console.log(x.indexOf(2));
+// console.log(x.indexOf(3));
+// console.log(x.indexOf(4));
+// console.log(x.indexOf(5));
+// console.log(x.indexOf(6));
+// console.log(x.indexOf(7));
+// console.log(x.indexOf(8));
+// console.log(x.indexOf(9));
+// console.log(x.indexOf(10));
+// x.splice(9);
+// console.log(x , x.length);
+
+// let classe = [];
+// classe.push("Beytullah","Gauthier","castor","Cactus");
+// console.log(classe, classe.length);
+
+// console.log(Math.random()*(classe.length))
+
+// console.log(classe[Math.floor(Math.random()*(classe.length))]);
+
+// classe.splice(classe.indexOf("Cactus"))
